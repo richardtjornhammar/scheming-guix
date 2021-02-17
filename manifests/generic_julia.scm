566a68b3a1ae1a52cbd21cd82cce236652f14620
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2021-02-17 10:25:18 +0100

    julia

---
+++ manifests/generic_julia.scm
@@ -0,0 +1,12 @@
+; guix environment --manifest=generic_julia.scm
+( specifications->manifest
+ '( "julia"
+    "julia-parsers"
+    "julia-fixedpointnumbers"
+    "julia-compat"
+    "julia-datastructures"
+    "julia-orderedcollections"
+    "julia-offsetarrays"
+    "emacs-julia-mode"
+  )
+)
