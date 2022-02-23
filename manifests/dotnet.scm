6340af305e0097d329a17ad5e17874111e0d5e30
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2022-02-23 10:37:08 +0100

    various envs

---
+++ manifests/dotnet.scm
@@ -0,0 +1,12 @@
+; guix environment --manifest=generic_julia.scm
+( specifications->manifest
+ '( "make"
+    "texlive"
+    "emacs-graphviz-dot-mode"
+    "dot2tex"
+    "texlive-pstool"
+    "graphviz"
+    "dotnet"
+    "ocaml"
+  )
+)
